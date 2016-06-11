fb8943342c057035db232b0fd14db11f22954898
Roberto Ortega <berto.ort@gmail.com>
2016-06-10 21:55:41 -0600

    add db call through query params

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,37 @@
 const express = require('express');
 const router = express.Router();
 const unirest = require('unirest');
+const knex = require('../db/knex');
+
+function Batches() {
+  return knex('batches')
+}
 
 router.get('/', (req, res) => {
   res.send('please enter a beer')
 });
 
 router.get('/beer', (req, res) => {
-  let quote = req.query.quote;
-  let tank = req.query.tank;
-  let time = req.query.time;
-  let date = req.query.date;
-  if (!time || !date) {
-    res.json({error: "make sure to include date and time"})
+  let quote = req.query.quote || '';
+  let tank = req.query.tank || '';
+  let time = req.query.time || '';
+  let date = req.query.date || '';
+  let beer_id = req.query.beer_id;
+  if (!beer_id) {
+    res.json({error: "make sure to include beer_id"})
   }
-  res.json({beerId: `fetching data for ${time} and ${date}`})
+  Batches().select()
+    .where('beer_id', beer_id)
+    // .orWhere('date', date)
+    // .orWhere('time', time)
+    .orWhere('tank', tank)
+    .orWhere('quote', quote)
+    .then(beer => {
+    res.json({
+      query: `fetching data for ${beer_id}`,
+      result: beer,
+    })
+  })
 });
 
 router.get('/:beer', (req, res, next) => {

1d17ba7d2720027edc183cfcbe68fe816147b380
Roberto Ortega <berto.ort@gmail.com>
2016-06-10 21:26:02 -0600

    add beer info and db mockup

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const routes = require('./routes/index');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -19,14 +21,14 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-router.get('/', function (req, res) {
+const express = require('express');
+const router = express.Router();
+const unirest = require('unirest');
+
+router.get('/', (req, res) => {
   res.send('please enter a beer')
-})
+});
 
-router.get('/:beer', function(req, res, next) {
-  var message = 'hello ' + req.params.beer
-  res.send(message);
+router.get('/beer', (req, res) => {
+  let quote = req.query.quote;
+  let tank = req.query.tank;
+  let time = req.query.time;
+  let date = req.query.date;
+  if (!time || !date) {
+    res.json({error: "make sure to include date and time"})
+  }
+  res.json({beerId: `fetching data for ${time} and ${date}`})
+});
+
+router.get('/:beer', (req, res, next) => {
+  let beer = req.params.beer;
+  let url = 'http://apis.mondorobot.com/beers/'
+  unirest.get(url + beer)
+    .end(response => {
+      let beerData = response.body.beer
+      res.json(beerData);
+    })
 });
 
 module.exports = router;

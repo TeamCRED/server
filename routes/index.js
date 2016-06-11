e080b736bfbdfa8f4cf1f384fced949ec21356b1
CJ R <hello@cjr.co.de>
2016-06-11 00:48:55 -0600

    added routes for everything. ever.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const unirest = require('unirest');
 const knex = require('../db/knex');
+const moment = require('moment');
 
 function Batches() {
   return knex('batches')
@@ -20,15 +21,60 @@ router.get('/beer', (req, res) => {
   let date = req.query.date || '';
   let beer_id = req.query.beer_id;
   if (!beer_id) {
-    res.json({error: "make sure to include beer_id"})
+    return res.json({
+      error: "make sure to include beer_id"
+    })
   }
-  Batches().select().where('beer_id', beer_id).then(beer => {
-    res.json({
-      beer_id: beer_id,
-      result: beer,
+  var query = Batches().where('beer_id', beer_id)
+
+  if (quote) query.andWhere('quote', quote);
+  if (date) query.andWhere('date', moment(date).format('MM-DD-YYYY'));
+  if (time) query.andWhere('time', time);
+  if (tank) query.andWhere('tank', tank);
+
+  query.then(batches => {
+    if (batches.length > 1) {
+      return res.json({
+        error: "Not enough information to find your brew. :("
+      })
+    } else {
+      var batch = batches[0];
+      console.log(batch)
+      Promise.all([
+        knex('employee_batches')
+        .where('batch_id', batch.id)
+        .join('employees', 'employees.id', 'employee_id').join('title', 'title.id', 'title_id'),
+        knex('user_batches').select('user_batches.user_id', 'user_batches.batch_id', 'users.first_name', 'users.last_name', 'users.id')
+        .where('batch_id', batch.id)
+        .join('users', 'users.id', 'user_id')
+      ]).then(function(result) {
+
+        res.json({
+          employees: result[0],
+          users: result[1]
+        });
+      })
+    }
+  }).catch(err => {
+    return res.json({
+      error: err
     })
   })
 });
+
+router.get('/awards', (req, res, next) =>{
+  if(req.user && req.user.id){
+    getAwards(req.user.id).then(function(awards){
+      res.json(awards)
+    })
+  }
+})
+
+router.get('/awards/:id', (req, res, next) => {
+  getAwards(req.params.id).then(function(awards){
+    res.json(awards)
+  })
+})
 
 router.get('/:beer', (req, res, next) => {
   let beer = req.params.beer;
@@ -39,5 +85,10 @@ router.get('/:beer', (req, res, next) => {
       res.json(beerData);
     })
 });
+
+function getAwards (id) {
+  return knex('user_awards').where('user_id', id).join('awards', 'awards.id', 'award_id')
+}
+
 
 module.exports = router;

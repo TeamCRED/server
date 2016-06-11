57fc84383ff3a62623d8776eb9779fc4e8796405
Roberto Ortega <berto.ort@gmail.com>
2016-06-10 21:07:29 -0600

    initial route setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('hello');
+router.get('/', function (req, res) {
+  res.send('please enter a beer')
+})
+
+router.get('/:beer', function(req, res, next) {
+  var message = 'hello ' + req.params.beer
+  res.send(message);
 });
 
 module.exports = router;
